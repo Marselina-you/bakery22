28aa37e5b8ed5d1896b52d511b477c6e9c4b8477
unknown <marselina88@list.ru>
2022-09-05 21:08:37 +0300

    дОБАВЛЕНИЕ ТОВАРА СТИЛИ

--- app/views/admin/product.php
+++ app/views/admin/product.php
@@ -5,7 +5,7 @@ include ROOT . '/views/layouts/admin_header.php';
     <div class="main">
   <div class="container-fluid col-xl-12 edit">
   	<?php include ROOT . '/views/layouts/admin_menu.php';?>
-<div class="edit__title title_padding fontSans size29px"><a href="/create" class="orange">До.бавить новый товар</a></div>
+<div class="edit__title title_padding fontSans size29px"><a href="/create" class="orange">Добавить новый товар</a></div>
 <div class="d-flex col-xl-12">
                     <?php foreach ($categories as $categoryItem): ?>
                 <div class="edit__title title_padding darkBlue fontSans size29px"><a href="categorys/<?php echo $categoryItem['id'];?>" class="col-xl-6 <?php if ($categoryId == $categoryItem['id']) echo 'active'; else echo'brownLight'; ?>"><?php echo $categoryItem['name'];?></a>
@@ -22,23 +22,42 @@ include ROOT . '/views/layouts/admin_header.php';
  <div class="edit-cart bottom_padding d-flex col-xl-6 col-lg-6">
          <div class="edit-cart__img col-xl-4"><img width ="80%" src="../template/images/<?php echo $product['photo']; ?>"></div>
          <div class="edit-cart__content d-flex flex-column">
-        <div class="edit-cart__title bottom_padding10 font-weight-bold size20px"><div class="burgundy  fontSegoePrint font-weight-bold size35px padding-15"><?php echo $product['name']; ?></div></div>
+        <div class="edit-cart__title bottom_padding10 font-weight-bold size20px"><div class="burgundy fontSegoePrint font-weight-bold size35px padding-15"><?php echo $product['name']; ?></div></div>
        <div class=""><div class="edit-cart__title bottom_padding10 size20px"><div class="brownDark justify-content-center font-weight-bold size24px"><?php echo $product['price']; ?><span class="rubl">₽</span></div></div>
-         <div class="edit__category bottom_padding10 size20px">Категория: пирожные</div></div>
-          <div class=""><div class="view-product-right-info__item brownDark bottom_padding fontSegoePrint size20px"><?php echo $product['weight']; ?> грамм</div>
-        <div class="bottom_padding d-flex flex-column brownDark fontSegoePrint size20px line-height1"><?php echo $product['description']; ?></div></div>
-          <div class=""><div class="">Ингредиенты: <div class="view-product-right-info__item brownDark fontSegoePrint size20px"><span class=""> -</span><?php echo $product['ing1']; ?></div>
-         <div class="view-product-right-info__item brownDark fontSegoePrint size20px"><span class=""> -</span><?php echo $product['ing2']; ?></div>
-          <div class="view-product-right-info__item brownDark fontSegoePrint size20px"><span class=""> -</span><?php echo $product['ing3']; ?></div>
+         </div>
+          <div class="edit__category size20px"><div class="view-product-right__status  brownGrey size16px"><?php echo $product['nal']; ?></div></div>
+          <div class="view-product-right-info__item brownDark bottom_padding fontSegoePrint size20px"><?php echo $product['weight']; ?> грамм</div>
+          <div class="edit__category size22px burgundyLight">Описание:</div>
+        <div class="bottom_padding10 d-flex flex-column brownDark fontSegoePrint size20px line-height1"><?php echo $product['description']; ?></div>
+
+         <div class="edit__category bottom_padding10"><div class="burgundyLight size22px">Ингредиенты:</div>
+            <div class="view-product-right-info__item size20px brownDark fontSegoePrint"><span class=""> -</span><?php echo $product['ing1']; ?></div>
+         <div class="view-product-right-info__item size20px brownDark fontSegoePrint"><span class=""> -</span><?php echo $product['ing2']; ?></div>
+          <div class="view-product-right-info__item size20px brownDark fontSegoePrint"><span class=""> -</span><?php echo $product['ing3']; ?></div>
       </div>
-         <div class="d-flex"><div class="size20px">Слоган:&nbsp; </div><div class="view-product-right-info__slogan brownLight size20px fontSegoePrint"> <?php echo $product['slogan']; ?></div>
-     </div></div>
-          <div class=""><div class="edit-cart__title bottom_padding10 size20px">Топ1: <div class="view-product-right-info__ing1 view-product-right-info__ing_padding d-flex justify-content-center align-items-center  white size24px fontTahoma"><?php echo $product['top1']; ?> </div></div>
-           <div class=""><div class="edit-cart__title bottom_padding10 size20px">Топ1: <div class="view-product-right-info__ing_padding d-flex justify-content-center align-items-center size24px fontTahoma"><?php echo $product['top2']; ?></div> </div>
-            <div class=""><div class="edit-cart__title bottom_padding10 size20px">Топ1:<div class="view-product-right-info__ing3 view-product-right-info__ing_padding d-flex justify-content-center align-items-center white size24px fontTahoma"> <?php echo $product['top3']; ?></div> </div>
-         <div class="edit__category bottom_padding10 size20px d-flex">Статус: <div class="view-product-right__status padding_status justify-content-center orange size24px"><?php echo $product['best']; ?></div></div>
-          <div class="edit__category bottom_padding10 size20px">Наличие: <div class="view-product-right__status  brownGrey size16px"><?php echo $product['nal']; ?></div></div>
-     </div></div></div>
+         <div class="d-flex"><div class="edit__category bottom_padding10 size22px burgundyLight">Слоган:&nbsp; </div><div class="brownLight size20px fontSegoePrint"> <?php echo $product['slogan']; ?></div>
+     </div>
+          <div class="block-top bottom_padding10">
+            <div class="block-top_top d-flex align-items-center">
+                <div class="burgundyLight edit-cart__title size20px">Топ 1:</div>
+                <div class="d-flex fontTahoma size20px icons__item_padding"> <?php echo $product['top1']; ?></div>
+            </div>
+           <div class="block-top_top d-flex align-items-center">
+            <div class="burgundyLight edit-cart__title size20px">Топ 2: </div>
+            <div class="d-flex fontTahoma size20px icons__item_padding"><?php echo $product['top2']; ?></div> 
+           </div>
+            <div class="block-top_top d-flex align-items-center">
+                <div class="burgundyLight edit-cart__title size20px">Топ 3:</div>
+                    <div class="d-flex fontTahoma size20px icons__item_padding"> <?php echo $product['top3']; ?></div>
+            </div>
+         </div>     
+         <div class="burgundyLight d-flex align-items-center bottom_padding">
+            <div class="burgundyLight edit-cart__title size22px">Статус:</div> 
+            <div class="view-product-right__status padding_status justify-content-center orange size20px  icons__item_padding"><?php echo $product['best']; ?></div>
+        </div>
+         
+     
+
 
 
 
@@ -58,4 +77,4 @@ include ROOT . '/views/layouts/admin_header.php';
 		
 	<?php endforeach; ?>
 </div></div></div></div>
-<?php include ROOT . '/views/layouts/footer_admin.php'; ?>
+<?php include ROOT . '/views/layouts/admin_footer.php'; ?>
--- app/views/admin_product/create.php
+++ app/views/admin_product/create.php
@@ -3,7 +3,8 @@ include ROOT . '/views/layouts/header.php'; ?>
  <div class="wrap-content">
     <div class="main">
   <div class="container-fluid col-xl-12 edit">
-<h1>АДМИНКА Добавление товарав</h1>
+  	<?php include ROOT . '/views/layouts/admin_menu.php';?>
+<div class="edit__title title_padding fontSans size29px orange">Добавление товарав</div>
  <?php if (isset($errors) && is_array($errors)): ?>
                 <ul>
                     <?php foreach ($errors as $error): ?>
@@ -11,47 +12,68 @@ include ROOT . '/views/layouts/header.php'; ?>
                     <?php endforeach; ?>
                 </ul>
             <?php endif; ?>
-
+<div class="d-flex flex-column col-xl-6 col-lg-6">
 <form action="#" method="post" enctype="multipart/form-data">
-	<p>Категории</p>
-	<select name="category_id">
-		 <?php if (is_array($categoriesList)): ?>
-			<?php foreach ($categoriesList as $category): ?>
-				<option value="<?php echo $category['id']; ?>" name="">
-					<?php echo $category['name']; ?>
-				</option>
+	<div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+		<div class="burgundyLight size22px">Категория</div>
+		    <select name="category_id" class="size24px admin_input">
+		    	<?php if (is_array($categoriesList)): ?>
+		    		<?php foreach ($categoriesList as $category): ?>
+		    			<option value="<?php echo $category['id']; ?>" name="">
+		    				<?php echo $category['name']; ?>
+		    					
+		    				</option>
 			<?php endforeach; ?>
 			 <?php endif; ?>
-	</select>
-	<p>Название товара</p>
-	<input type="text" name="name" placeholder="" value="">
-	<p>Стоимость</p>
-	<input type="text" name="price" placeholder="" value="">
+	</select></div>
+	<div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+		<div class="burgundy fontSegoePrint font-weight-bold size24px">Название товара</div>
+	<input type="text" name="name" placeholder="" value="" class="size24px admin_input"></div>
+	<div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+	<div class="brownDark justify-content-center font-weight-bold size24px">Стоимость</div>
+	<input type="text" name="price" placeholder="" value="" class="size24px admin_input"></div>
 
+<div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+	<div class="">Изображение товара</div>
+	<input type="file" name="image" placeholder="" value="" class="size24px admin_input"></div>
+	<div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+	<div class="view-product-right-info__item brownDark bottom_padding fontSegoePrint size20px">Вес товара</div>
+	<input type="text" name="weight" placeholder="" value="" class="size24px admin_input"></div>
+	<div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+	<div class="bottom_padding10 d-flex flex-column brownDark fontSegoePrint size20px line-height1">Oписание</div>
+	<textarea name="description"></textarea></div>
+<div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+	<div class="burgundyLight size22px">Ингредиент1</div>
+	<input type="text" name="ing1" placeholder="" value=""  class="size24px admin_input"></div>
+<div class="d-flex align-items-center view-product-right-info__item justify-content-between">	
+	<div class="burgundyLight size22px">Ингредиент2</div>
+	<input type="text" name="ing2" placeholder="" value=""  class="size24px admin_input"></div>
+<div class="d-flex align-items-center view-product-right-info__item justify-content-between">	
+	<div class="burgundyLight size22px">Ингредиент3</div>
+	<input type="text" name="ing3" placeholder="" value=""  class="size24px admin_input"></div>
+<div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+	<div class="brownLight size20px fontSegoePrint">Слоган</div>
+	<input type="text" name="slogan" placeholder="" value=""  class="size24px admin_input"></div>
+<div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+	<div class="burgundyLight edit-cart__title size20px"  class="size24px admin_input">Топ 1</div>
+	<input type="text" name="top1" placeholder="" value=""  class="size24px admin_input"></div>
+<div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+	<div class="burgundyLight edit-cart__title size20px">Топ 2</div>
+	<input type="text" name="top2" placeholder="" value=""  class="size24px admin_input"></div>
+<div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+	<div class="burgundyLight edit-cart__title size20px">Топ 3</div>
+	<input type="text" name="top3" placeholder="" value=""  class="size24px admin_input"></div>
+<div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+	<div class="burgundyLight edit-cart__title size20px">Статус</div>
+	<select name="category_id" class="size24px admin_input">
+		    	
+		    			<option value="" name="В наличии">В наличии</option>
+		    			<option value="" name="Нет">Нет</option></select>
 
-	<p>Изображение товара</p>
-	<input type="file" name="image" placeholder="" value="">
-	<p>Вес товара</p>
-	<input type="text" name="weight" placeholder="" value="">
-	<p>Детальное описание</p>
-	<textarea name="description"></textarea>
-
-	<p>Ингредиент1</p>
-	<input type="text" name="ing1" placeholder="" value="">
-	<p>Ингредиент2</p>
-	<input type="text" name="ing2" placeholder="" value="">
-	<p>Ингредиент3</p>
-	<input type="text" name="ing3" placeholder="" value="">
-	<p>Слоган</p>
-	<input type="text" name="slogan" placeholder="" value="">
-	<p>Топ 1</p>
-	<input type="text" name="top1" placeholder="" value="">
-	<p>Топ 2</p>
-	<input type="text" name="top2" placeholder="" value="">
-	<p>Топ 3</p>
-	<input type="text" name="top3" placeholder="" value="">
-
+		    		</div>
+		    	
 
 	<input type="submit" name="submit" value="Сохранить">
-</form></div></div></div>
-<?php include ROOT . '/views/layouts/footer_admin.php'; ?>
+</form></div>
+</div></div></div>
+<?php include ROOT . '/views/layouts/admin_footer.php'; ?>
