2e6e8b40dafbf8e26c002106f1919cdc22b6508a
unknown <marselina88@list.ru>
2022-09-08 19:11:05 +0300

    ничего не понимаю

--- app/controllers/AdminProductController.php
+++ app/controllers/AdminProductController.php
@@ -85,11 +85,11 @@ $productsList = Product::getProductsList();
 
 
 			}
-	public function actionUpdate($id)
+	 public function actionUpdate($id)
     {
         // Проверка доступа
         self::checkAdmin();
-        $userId = User::checkLoggedSite();
+ $userId = User::checkLoggedSite();
         $user = User::getUserById($userId);
         // Получаем список категорий для выпадающего списка
         $categoriesList = Category::getCategoriesListAdmin();
@@ -101,23 +101,24 @@ $productsList = Product::getProductsList();
         if (isset($_POST['submit'])) {
             // Если форма отправлена
             // Получаем данные из формы редактирования. При необходимости можно валидировать значения
-            $options['id'] = $_POST['id'];
-            $options['name'] = $_POST['name'];
+           
             $options['category_id'] = $_POST['category_id'];
-			$options['price'] = $_POST['price'];
-			$options['weight'] = $_POST['weight'];
-			$options['description'] = $_POST['description'];
-			$options['ing1'] = $_POST['ing1'];
-			$options['ing2'] = $_POST['ing2'];
-			$options['ing3'] = $_POST['ing3'];
-			$options['slogan'] = $_POST['slogan'];
-			$options['top1'] = $_POST['top1'];
-			$options['top2'] = $_POST['top2'];
-			$options['top3'] = $_POST['top3'];
-			$options['best'] = $_POST['best'];
-			$options['nal'] = $_POST['nal'];
-			$options['new_picture'] = $_FILES['new_picture']['name']; 
             
+            $options['name'] = $_POST['name'];
+            $options['price'] = $_POST['price'];
+            $options['weight'] = $_POST['weight'];
+            
+            $options['description'] = $_POST['description'];
+            $options['ing1'] = $_POST['ing1'];
+            $options['ing2'] = $_POST['ing2'];
+            $options['ing3'] = $_POST['ing3'];
+            $options['slogan'] = $_POST['slogan'];
+            $options['top1'] = $_POST['top1'];
+            $options['top2'] = $_POST['top2'];
+            $options['top3'] = $_POST['top3'];
+            $options['nal'] = $_POST['nal'];
+            
+            $options['best'] = $_POST['best'];
 
             // Сохраняем изменения
             if (Product::updateProductById($id, $options)) {
@@ -127,13 +128,13 @@ $productsList = Product::getProductsList();
                 // Проверим, загружалось ли через форму изображение
                  // Если загружалось, переместим его в нужную папке, дадим новое имя
                 if (is_uploaded_file($_FILES["new_picture"]["tmp_name"])) {
-                  move_uploaded_file($_FILES['new_picture']['tmp_name'], $_SERVER['DOCUMENT_ROOT'] .'/upload/images/products/'.$_FILES['new_picture']['name']);
+                   move_uploaded_file($_FILES["new_picture"]["tmp_name"], $_SERVER['DOCUMENT_ROOT'] . "/upload/images/products/{$id}.jpg");
                 }
                 
             }
 
             // Перенаправляем пользователя на страницу управлениями товарами
-            header("Location: /admin/catalog");
+            header("Location: /admin/product");
         }
 
         // Подключаем вид
