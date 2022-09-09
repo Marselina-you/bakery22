53036762aff477a5175dc6b6f9f15b2b58e5aae9
unknown <marselina88@list.ru>
2022-09-09 19:31:07 +0300

    все прекрасненько

--- app/views/admin_product/update.php
+++ app/views/admin_product/update.php
@@ -4,29 +4,18 @@ include ROOT . '/views/layouts/header.php'; ?>
     <div class="main">
   <div class="container-fluid col-xl-12 edit">
     <?php include ROOT . '/views/layouts/admin_menu.php';?>
-<div class="container">
-        <div class="row">
+<div class="wrap-content">
+    <div class="main">
+  <div class="container-fluid col-xl-12 edit">
+    <?php include ROOT . '/views/layouts/admin_menu.php';?>
+<div class="edit__title title_padding fontSans size29px orange">Редактировать товар #<?php echo $id; ?></div>
 
-            <br/>
+    <div class="d-flex flex-column col-xl-8 col-lg-8 form-create form_padding">      
 
-            <div class="breadcrumbs">
-                <ol class="breadcrumb">
-                    <li><a href="/admin">Админпанель</a></li>
-                    <li><a href="/admin/product">Управление товарами</a></li>
-                    <li class="active"><a href="/admin/product/update">Редактировать товар</a></li>
-                </ol>
-            </div>
-
-
-            <h4>Редактировать товар #<?php echo $id; ?></h4>
-
-            <br/>
-
-            <div class="col-lg-4">
-                <div class="login-form">
-                    <form action="#" method="post" enctype="multipart/form-data">
-<p>Категории</p>
-    <select name="category_id">
+            <form action="#" method="post" enctype="multipart/form-data">
+<div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+        <div class="burgundyLight size22px">Категория</div>
+    <select name="category_id" class="size24px admin_input">
          <?php if (is_array($categoriesList)): ?>
             <?php foreach ($categoriesList as $category): ?>
                 <option value="<?php echo $category['id']; ?>" name="">
@@ -34,45 +23,70 @@ include ROOT . '/views/layouts/header.php'; ?>
                 </option>
             <?php endforeach; ?>
              <?php endif; ?>
-    </select>
-                        <p>Название товара</p>
-                        <input type="text" name="name" placeholder="" value="<?php echo $product['name']; ?>">
-
-                </br>        
-
-                <p>Стоимость, $</p>
-                        <input type="text" name="price" placeholder="" value="<?php echo $product['price']; ?>">
-
+    </select></div>
+    <div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+        <div class="burgundy fontSegoePrint font-weight-bold size35px">Название товара</div>
+    <input type="text" name="name" placeholder="" value="<?php echo $product['name']; ?>"  class="size24px admin_input"></div>
+    <div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+    <div class="brownDark justify-content-center font-weight-bold size24px">Стоимость</div>
+    <input type="text" name="price" placeholder="" class="size24px admin_input" value="<?php echo $product['price']; ?>"></div>                  
+    <div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+    <div class="view-product-right-info__item brownDark bottom_padding fontSegoePrint size20px">Вес товара</div>
+    <input type="text" name="weight" placeholder="" class="size24px admin_input" value="<?php echo $product['weight']; ?>"></div>             
+<div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+    <div class="view-product-right-info__title bottom_padding burgundy size29px fontTahoma padding-15">Oписание</div>
+    <textarea name="description" class="textarea_admin size24px admin_input"><?php echo $product['description']; ?></textarea></div>
+    <div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+    <div class="burgundyLight size22px">Ингредиент1</div>
+    <input type="text" name="ing1" placeholder="" class="size24px admin_input"  value="<?php echo $product['ing1']; ?>"></div>
+    <div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+    <div class="burgundyLight size22px">Ингредиент2</div>
+    <input type="text" name="ing2" placeholder="" class="size24px admin_input"  value="<?php echo $product['ing2']; ?>"></div>  
+    <div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+    <div class="burgundyLight size22px">Ингредиент3</div>
+    <input type="text" name="ing3" placeholder="" class="size24px admin_input"  value="<?php echo $product['ing3']; ?>"></div>             
+<div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+    <div class="view-product-right-info__slogan view-product-right-info_padding brownLight size35px fontSegoePrint">Слоган</div>
+    <input type="text" name="slogan" placeholder="" value="<?php echo $product['slogan']; ?>"  class="size24px admin_input"></div>
+<div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+    <div class="view-product-right-info__title bottom_padding burgundy size29px fontTahoma">Топ 1</div>
+    <input type="text" name="top1" placeholder="" value="<?php echo $product['top1']; ?>"  class="size24px admin_input"></div>
+<div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+    <div class="view-product-right-info__title bottom_padding burgundy size29px fontTahoma">Топ 2</div>
+    <input type="text" name="top2" placeholder="" value="<?php echo $product['top2']; ?>"  class="size24px admin_input"></div>
+    <div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+    <div class="view-product-right-info__title bottom_padding burgundy size29px fontTahoma">Топ 3</div>
+    <input type="text" name="top3" placeholder="" value="<?php echo $product['top3']; ?>"  class="size24px admin_input"></div>
                        
+    <div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+    <div class="view-product-right__status padding_status justify-content-center orange size24px">Статус</div>
+    <select name="best" class="size24px admin_input">
+                
+                        <option value="Бестселлер">Бестселлер</option>
+                        <option value="Новинка!">Новинка!</option>
+                        <option value="Выгодно">Выгодно</option>
+                        <option value="Уже в продаже">Уже в продаже</option></select>
 
-                      
-                    <p>Вес товара</p>
-    <input type="text" name="weight" placeholder="" value="<?php echo $product['weight']; ?>">    
+                    </div>
+                <div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+    <div class="view-product-right__status  brownGrey size16px">Наличие</div>
+    <select name="nal" class="size24px admin_input">
+                
+                        <option value="В наличии">В наличии</option>
+                        <option value="Нет">Нет</option></select>
 
-                        <p>Детальное описание</p>
-                        <textarea name="description"><?php echo $product['description']; ?></textarea>
-                        <p>Изображение товара</p>
+                    </div>
+
+                 
+                      <div class="d-flex align-items-center view-product-right-info__item justify-content-between">
+                          <div class="burgundyLight edit-cart__title size20px">Изображение товара</div>
                         <img src="/upload/images/products/<?php echo $product['new_picture']; ?>" width="200" alt="" />
                         <input type="file" name="new_picture" placeholder="" value="<?php echo $product['new_picture']; ?>">
-                        <br/><br/>
-                        <p>Ингредиен1</p>
-    <input type="text" name="ing1" placeholder="" value="<?php echo $product['ing1']; ?>">
-    <p>Ингредиент2</p>
-    <input type="text" name="ing2" placeholder="" value="<?php echo $product['ing2']; ?>">
-    <p>Ингредиент3</p>
-    <input type="text" name="ing3" placeholder="" value="<?php echo $product['ing3']; ?>">
-    <p>Слоган</p>
-    <input type="text" name="slogan" placeholder="" value="<?php echo $product['slogan']; ?>">
-    <p>Топ 1</p>
-    <input type="text" name="top1" placeholder="" value="<?php echo $product['top1']; ?>">
-    <p>Топ 2</p>
-    <input type="text" name="top2" placeholder="" value="<?php echo $product['top2']; ?>">
-    <p>Топ 3</p>
-    <input type="text" name="top3" placeholder="" value="<?php echo $product['top3']; ?>">
-    <p>best</p>
-    <input type="text" name="best" placeholder="" value="<?php echo $product['best']; ?>">
-    <p>nal</p>
-    <input type="text" name="nal" placeholder="" value="<?php echo $product['nal']; ?>">
+                       </div>
+                        
+   
+        
+  
 
 
                      
@@ -86,7 +100,8 @@ include ROOT . '/views/layouts/header.php'; ?>
 
                      
                         
-                        <input type="submit" name="submit" class="btn btn-default" value="Сохранить">
+                      <div class="d-flex justify-content-end">
+    <input type="submit" name="submit" value="Сохранить" class="btn btn-success size24px"></div>
                         
                         
                     </form>
